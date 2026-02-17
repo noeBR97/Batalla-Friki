3b0bd34d982e665298f948efcfc0a47382975dee
Noelia Barrionuevo <noe.br97@gmail.com>
2026-02-17 17:13:52 +0100

    personajesModule en appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { PersonajesModule } from './personajes/personajes.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/batalla-friki'), UsuariosModule, AuthModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/batalla-friki'), UsuariosModule, AuthModule, PersonajesModule],
   controllers: [],
   providers: [],
 })

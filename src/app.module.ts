44671a4f9b45b724896eaca5c4cb5870cec0769f
Noelia Barrionuevo <noe.br97@gmail.com>
2026-02-17 13:15:52 +0100

    importaos mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsuariosModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/batalla-friki'), UsuariosModule, AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

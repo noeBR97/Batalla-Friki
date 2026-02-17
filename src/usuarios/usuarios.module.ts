ea7bee8d1575aca187207540bc933754d918d104
Noelia Barrionuevo <noe.br97@gmail.com>
2026-02-17 13:17:22 +0100

    fix bucle de importaciones entre modulos auth y usuarios

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
+import { UsuarioSchema, Usuario } from './entities/usuario.entity';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  exports: [UsuariosService],
   controllers: [UsuariosController],
   providers: [UsuariosService],
+  imports: [
+    forwardRef(() => AuthModule),
+    MongooseModule.forFeature([
+      {
+        name: Usuario.name, //Nombre de la clase del modelo: 'Usuario'.
+        schema: UsuarioSchema, //Esquema del modelo.
+      },
+    ]),
+  ],
 })
 export class UsuariosModule {}
+
